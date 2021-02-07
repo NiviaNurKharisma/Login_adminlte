b8a95ceed63d225bf90e6ff116075f7e37a18e68
Nivia Nur Kharisma <53246336+NiviaNurKharisma@users.noreply.github.com>
2021-02-07 12:58:12 +0700

    Add files via upload

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+use Illuminate\Database\Seeder; 
+use Illuminate\Support\Facades\Hash;
+use App\Models\User; 
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // \App\Models\User::factory(10)->create();
+        User::create([
+            "name" => "Sopingi",
+            "email" => "sopingi@udb.ac.id",
+            "hak_akses" =>"administrator",
+            "password" => Hash::make('admin')
+            ]);
+    }
+}
